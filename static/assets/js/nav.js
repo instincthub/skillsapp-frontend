4699ce0e54436f391aae4c851c0e83039bc7fe06
MicroHub69 <msodq2018@gmail.com>
2022-05-11 14:53:26 +0100

    Added label for tracks

--- static/assets/js/nav.js
+++ static/assets/js/nav.js
@@ -4,6 +4,29 @@ const miniMenu = document.querySelector(".mini_menu");
 dropDown.addEventListener("click", () => {
   miniMenu.classList.toggle("active");
 });
-dropDown.addEventListener("mouseenter", () => {
-  miniMenu.classList.toggle("active");
-});
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".navlinks");
+  const navLinks = document.querySelectorAll(".navlinks li");
+
+  //toggle nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+
+    //Animation Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 9 + 0.2
+        }s`;
+      }
+    });
+
+    //bURGER aNIMATION
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
